1a6da967a22805691c741aca3d92b68afab077ac
Gaurav Saraf <gaurav@gauravsaraf.com>
2012-10-06 10:06:08 +0530

    First commit: Rails new www

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Www::Application.config.secret_token = 'fbd3d283e1f45eee43f75ef1fb13fbab89831a02121550d7878a4f04e3ea996d47b731d86bbe8dc011656af1ee43be7c91b1d24d835c9ae8b17851e8ac1ccc71'
